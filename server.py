bbb960f3578078fcadeed2f097dc2a30cdd39f47
Nancy <nancy050628@qq.com>
2025-05-02 19:23:41 +0800

    Initial the total operations count, READ operations count, GET operations count, PUT operations count, Error count, and Total connected clients

--- server.py
+++ server.py
@@ -6,5 +6,12 @@ class TupleSpaceServer:
         self.port = port
         self.tuple_space = {}
         self.lock = threading.Lock()
+        self.total_operations = 0
+        self.read_operations = 0
+        self.get_operations = 0
+        self.put_operations = 0
+        self.errors = 0
+        self.total_clients = 0
+
 
 
