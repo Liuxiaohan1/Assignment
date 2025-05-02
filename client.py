d54b9795a89626f0662adc7dc60a8a56ee254a7a
Nancy <nancy050628@qq.com>
2025-05-02 17:53:32 +0800

    Receive response and decode

--- client.py
+++ client.py
@@ -31,7 +31,9 @@ def client(hostname, port, filename):
             message_length = len(combined)
             message = f"{message_length:03}{combined}"
             s.send(message.encode())
-            
+            response = s.recv(1024).decode()
+           
+
             
 
 
