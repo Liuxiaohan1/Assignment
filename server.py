d86809be31cd496f7461fec624fa3092a94f6194
Nancy <nancy050628@qq.com>
2025-05-02 19:42:27 +0800

    Send response back to client

--- server.py
+++ server.py
@@ -59,6 +59,7 @@ class TupleSpaceServer:
                         else:
                             self.tuple_space[key] = value
                             response = f"{len(f'OK ({key}, {value}) added'):03} OK ({key}, {value}) added"
+                client_socket.send(response.encode())
 
                     
                     
