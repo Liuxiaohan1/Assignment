67770b6be01b2f23fd7cabab24274ee4ed7d20aa
Nancy <nancy050628@qq.com>
2025-05-02 19:49:36 +0800

    Get port from command line, Create server instance, and start server

--- server.py
+++ server.py
@@ -85,6 +85,15 @@ class TupleSpaceServer:
         while True:
             client_socket, addr = self.server_socket.accept()
             threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python server.py <port>")
+        sys.exit(1)
+    port = int(sys.argv[1])
+    server = TupleSpaceServer(port)
+    server.start()
+    
 
 
 
